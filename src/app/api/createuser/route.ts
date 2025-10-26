7a05f76af37c7538fbe5ba39d355f6e472594bd4
chimpy7 <aleksandar199@hotmail.com>
2025-10-26 20:37:26 +0100

    santized routes added checks

--- src/app/api/createuser/route.ts
+++ src/app/api/createuser/route.ts
@@ -12,8 +12,16 @@ export async function POST(req: Request) {
         if(data.email == null || data.password == null ||data.name == ""){
             return NextResponse.json({error:"Email and password are required"},{status:400});
         }
+
+        if (data.password.length < 6) {
+            return NextResponse.json({ error: "Password must be at least 6 characters long" }, { status: 400 });
+        }
+        if (data.name.length < 3) {
+            return NextResponse.json({ error: "Name must be at least 3 characters long" }, { status: 400 });
+        }
+        
         const newUser = await User.create(data);
-        return NextResponse.json(newUser, { status: 201 });
+        return NextResponse.json({ status: 201 });
     } catch (err) {
         console.error("Error creating user:", err);
         return NextResponse.json({ error: "Server error" }, { status: 500 });
